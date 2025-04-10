d0a9bf8974f74bc7e711a5ea7efef39768a630e7
Chao <chao.weng@appdynamics.com>
2025-04-10 23:29:19 +0800

    wip

--- cmd/mdatagen/internal/log_test.go
+++ cmd/mdatagen/internal/log_test.go
@@ -6,9 +6,8 @@ package internal
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLogNameRender(t *testing.T) {
